046583c849d99e5208cc33b586c5cb9dcf94e92a
Alexandre Roussel <roussela27@gmail.com>
2020-07-05 16:48:06 +0200

    commentaire PopUp

--- Android/AppMaree/app/src/main/java/com/example/appmaree/PopUp.kt
+++ Android/AppMaree/app/src/main/java/com/example/appmaree/PopUp.kt
@@ -12,24 +12,40 @@ import androidx.fragment.app.FragmentActivity
 import kotlin.collections.ArrayList
 import kotlin.random.Random
 
+/**
+ * Créer une classe PopUp qui permet d'afficher une fenêtre par dessus l'application
+ * la fenêtre de popup est utilisé pour passer des publicités pour les sponsors de l'association
+ * la création de la poup demande l'activité sur laquelle afficher la popup
+ */
 class PopUp private constructor(activity: FragmentActivity?) : PopupWindow(activity)  {
+
+    // définie le temps entre chaque pubs
+    private val tempsEntrePub: Long = 45000
     private var image: ImageView?
     private var imageArray: ArrayList<Int>
-    private val tempsEntrePub: Long = 45000
     private val handler : Handler
     private val view : View? = activity?.layoutInflater?.inflate(R.layout.activity_popup, null)
     private var pub : Boolean = true
 
-
+    /**
+     * Permet de transformer la classe PopUp en Singleton
+     * Une seule instance de popup existe pour éviter d'avoir plusieurs images qui
+     * apparaissent sur l'écran en même temps
+     */
     companion object {
         private lateinit var instance: PopUp
-
         fun getInstance(activity: FragmentActivity?): PopUp {
             if(!::instance.isInitialized)  instance = PopUp(activity)
             return instance
         }
     }
 
+    /**
+     * Code éxécuté lors de l'instanciation de la classe PopUp
+     * Affecte la fonction sur le bouton fermer pour fermer la popup et
+     * en rappeler une nouvelle 45s après
+     * Créer le tableau des images en fonction des poids (utilise la classe ListeSponsor dans Sponsor.kt)
+     */
     init {
         contentView = view
         handler=Handler()
@@ -46,11 +62,9 @@ class PopUp private constructor(activity: FragmentActivity?) : PopupWindow(activ
             }
 
         }
+        image = view?.findViewById(R.id.popupImage)
 
-        image = view?.findViewById<ImageView>(R.id.popupImage)
-
-
-        val arraySponsor = ListeSponsor()
+        val arraySponsor = ListeSponsor().listeSponsor
 
         imageArray = ArrayList()
         for (sponsor in arraySponsor) {
@@ -61,64 +75,26 @@ class PopUp private constructor(activity: FragmentActivity?) : PopupWindow(activ
         handler.postDelayed({
             show()
         }, tempsEntrePub)
-
     }
 
-
+    /**
+     * Affiche une image aléatoirement parmis le tableau des sponsors
+     * est appelé 45s après l'ouverture de la page horaire et
+     * 45s après la fermeture de la popup précédente
+     */
     fun show() {
         image?.setImageResource(imageArray.get(Random.nextInt(imageArray.size)))
         showAtLocation(view, BOTTOM, 0, 0)
         pub = true
     }
 
+    /**
+     * Affiche l'image passée en paramêtre (adresse de l'image)
+     * Utile pour la page Services
+     */
     fun showSponsor(resource : Int) {
         image?.setImageResource(resource)
         showAtLocation(view, BOTTOM, 0, 0)
         pub = false
     }
-
 }
-/*
-        imageArray = intArrayOf(
-            R.drawable.appd,
-            R.drawable.atelier_mobile_bateau_2,
-            R.drawable.axe_sail_2,
-            R.drawable.boulangerie2_1,
-            R.drawable.bouteille_a_la_mer_2,
-            R.drawable.ca,
-            R.drawable.cafe_paix_1,
-            R.drawable.cambuse_2,
-            R.drawable.cap_marine_1,
-            R.drawable.carteret_marine_2,
-            R.drawable.charles_optique_2,
-            R.drawable.cn_dielette_2,
-            R.drawable.coccimarket_2,
-            R.drawable.comptoir_mer,
-            R.drawable.cotentin_surf_club_2,
-            R.drawable.ed_and_coif_2,
-            R.drawable.hague_sud_plongee_1,
-            R.drawable.hague_sud_plongee,
-            R.drawable.intermarche_4,
-            R.drawable.lagoon_1,
-            R.drawable.la_falaise,
-            R.drawable.la_falaise_1	,
-            R.drawable.les_travailleurs_de_l_amer_2,
-            R.drawable.le_bouche_a_oreille_1,
-            R.drawable.le_douet,
-            R.drawable.le_vent_mer,
-            R.drawable.long_john_silver,
-            R.drawable.maison_du_biscuit_2,
-            R.drawable.manche_iles_express_1,
-            R.drawable.mecanautic_1,
-            R.drawable.peche_and_co_2,
-            R.drawable.peugeot_leroutier,
-            R.drawable.sciotot_2,
-            R.drawable.snsm,
-            R.drawable.taxi_4_,
-            R.drawable.taxi_durel_2,
-            R.drawable.taxi_thomine_2,
-            R.drawable.tcheu_ptit_louis_1,
-            R.drawable.ti_pou_bar_1,
-            R.drawable.voilerie_des_isles,
-            R.drawable.weldom_2)
-        */
