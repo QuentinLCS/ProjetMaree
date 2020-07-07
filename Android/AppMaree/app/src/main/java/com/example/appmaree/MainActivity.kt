110dd034be331dc07bba42ae0ad2d26ff964a05e
Tristan <tristandanger@hotmail.fr>
2020-07-07 15:07:17 +0200

    Petit dégradé blanc en bas

--- Android/AppMaree/app/src/main/java/com/example/appmaree/MainActivity.kt
+++ Android/AppMaree/app/src/main/java/com/example/appmaree/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : AppCompatActivity() {
                     }
                     BottomSheetBehavior.STATE_COLLAPSED ->{
                         var param = home.layoutParams
-                        param.height=(75*scale).toInt()
+                        param.height=(125*scale).toInt()
                         home.layoutParams=param
                     }
                     BottomSheetBehavior.STATE_DRAGGING -> { }
@@ -77,7 +77,7 @@ class MainActivity : AppCompatActivity() {
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
                 if(slideOffset>0){
                     var param = home.layoutParams
-                    param.height=(75*scale*(1-slideOffset)).toInt()
+                    param.height=(125*scale*(1-slideOffset)).toInt()
                     param.width==param.height
                     home.layoutParams=param}
             }
